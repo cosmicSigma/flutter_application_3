8a07af5523032686ac6f1ae4f998cb93d97d5dd4
cosmicSigma <abdurashidxodiyev17@gmail.com>
2023-04-19 09:24:41 +0500

    mustaqil_ish_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,9 +22,74 @@ class Lesson extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
       body: Center(
-        child: null,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: SizedBox(
+            height: 220,
+            width: 250,
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 40, left: 25),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        height: 60,
+                        width: 60,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Colors.pinkAccent,
+                            borderRadius: BorderRadius.circular(15)
+                            ),
+                            child: Center(child: Text("</>",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),)),
+                          ),
+                          
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Text(
+                          "Engineering",
+                          style: TextStyle(
+                            fontSize: 25
+                          ),
+                          ),
+                      )
+                    ]
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 40),
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: SizedBox(
+                        height: 60,
+                        width: 220,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "8 open positions",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                    )
+              ],
+              )
+            ),
+          )
       ),
     );
   }
