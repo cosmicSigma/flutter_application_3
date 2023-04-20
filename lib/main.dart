300344e85b006ee459083cc9a946b1e13bf9df4d
cosmicSigma <abdurashidxodiyev17@gmail.com>
2023-04-20 07:17:45 +0500

    hometask_5

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,219 +27,99 @@ class Lesson extends StatelessWidget {
       body: Center(
         child: DecoratedBox(
           decoration: BoxDecoration(
-              color: Colors.white54, borderRadius: BorderRadius.circular(30)),
+              color: Color.fromRGBO(255, 255, 255, 0.5), borderRadius: BorderRadius.circular(30)),
           child: SizedBox(
-            height: 470,
+            height: 700,
             width: 350,
-            child: Padding(
-              padding: const EdgeInsets.all(25),
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20)),
-                child: SizedBox(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+            child: Column(
+              children: [
+                const Text(
+                  r"""Search educational resources"""
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
                     children: [
-                      DecoratedBox(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Color(0xffA6ADD2),
-                            width: 1,
-                            style: BorderStyle.solid
+                      Padding(
+                        padding: const EdgeInsets.all(0),
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(40)
                           ),
-                          borderRadius: BorderRadius.circular(50)
-                        ),
-                        child: SizedBox(
-                          height: 100,
-                          width: 100,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              Icon(
-                                Icons.photo_outlined,
-                                size: 40,
-                                color: Color(0xff1B255A),
-                                ),
-                              Text(
-                                "BASIC",
-                                style: TextStyle(
-                                  fontSize: 17,
-                                  color: Color(0xff1B255A),
-                                  fontWeight: FontWeight.bold
-                                ),
-                                )
-                            ],
+                          child: const SizedBox(
+                            height: 40,
+                            width: 40,
                           ),
-                        ),
+                          ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20)
+                            ),
+                            child: SizedBox(
+                              height: 300,
+                              child: Padding(
+                                padding: const EdgeInsets.only(top: 30,bottom: 60),
+                                child: Column(
+                                  children: [
+                                    Transform.rotate(
+                                      angle: pi * 3/2,
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(bottom: 50, top: 90),
+                                        child: Column(
+                                          children: [
+                                            Text("Seatch form"),
+                                            Text("Share suggestions, ask questions, and connect with other users and top contributors in the Search community forum.")
+                                          ],
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              ),
+                            ),
+                          ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20)
+                            ),
+                            child: SizedBox(
+                              height: 300,
+                              ),
+                            ),
+                          ),
                       ),
                       Padding(
-                        padding: EdgeInsets.only(left: 110, top: 20),
-                        child: Row(
-                          children: const [
-                            Text(
-                              "KR",
-                              style: TextStyle(
-                                color: Color(0xff1B255A),
-                                fontSize: 17
-                              ),
-                            ),
-                            Text(
-                              "199",
-                              style: TextStyle(
-                                color: Color(0xff1B255A),
-                                fontWeight: FontWeight.bold,
-                                fontSize: 25
-                              ),
-                              )
-                          ],
-                        ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.only(bottom: 20),
-                          child: Text(
-                            "NOK/MONTH",
-                            style: TextStyle(
-                              fontSize: 17,
-                              color: Color(0xff1B255A)
-                            ),
+                        padding: const EdgeInsets.all(0),
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(40)
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              DecoratedBox(
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Color(0xffD2D9F3),
-                                    width: 1.2,
-                                    style: BorderStyle.solid
-                                  ),
-                                  borderRadius: BorderRadius.circular(17)
-                                ),
-                                child: const SizedBox(
-                                  height: 50,
-                                  width: 60,
-                                  child: Center(
-                                    child: Text(
-                                      "10",
-                                      style: TextStyle(
-                                        color: Color(0xff1B255A),
-                                        fontSize: 23
-                                      ),
-                                      ),
-                                  ),
-                                ),
-                                ),
-                                DecoratedBox(
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Color(0xffD2D9F3),
-                                    width: 1.2,
-                                    style: BorderStyle.solid
-                                  ),
-                                  color: Color(0xff1B255A),
-                                  borderRadius: BorderRadius.circular(17)
-                                ),
-                                child: const SizedBox(
-                                  height: 50,
-                                  width: 60,
-                                  child: Center(
-                                    child: Text(
-                                      "25",
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 23
-                                      ),
-                                      ),
-                                  ),
-                                ),
-                                ),
-                                DecoratedBox(
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Color(0xffD2D9F3),
-                                    width: 1.2,
-                                    style: BorderStyle.solid
-                                  ),
-                                  borderRadius: BorderRadius.circular(17)
-                                ),
-                                child: const SizedBox(
-                                  height: 50,
-                                  width: 60,
-                                  child: Center(
-                                    child: Text(
-                                      "50",
-                                      style: TextStyle(
-                                        color: Color(0xff1B255A),
-                                        fontSize: 23
-                                      ),
-                                      ),
-                                  ),
-                                ),
-                                ),
-                                DecoratedBox(
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Color(0xffD2D9F3),
-                                    width: 1.2,
-                                    style: BorderStyle.solid
-                                  ),
-                                  borderRadius: BorderRadius.circular(17)
-                                ),
-                                child: const SizedBox(
-                                  height: 50,
-                                  width: 60,
-                                  child: Center(
-                                    child: Text(
-                                      "100",
-                                      style: TextStyle(
-                                        color: Color(0xff1B255A),
-                                        fontSize: 23
-                                      ),
-                                      ),
-                                  ),
-                                ),
-                                )
-                            ],
-                          ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.only(bottom: 25),
-                          child: Text(
-                            "Downloads per month",
-                            style: TextStyle(
-                              color: Color(0xff1B255A),
-                              fontSize: 17
-                            ),
+                          child: const SizedBox(
+                            height: 40,
+                            width: 40,
                           ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(10),
-                            child: DecoratedBox(
-                              decoration: BoxDecoration(
-                                color: Color(0xff1B255A),
-                                borderRadius: BorderRadius.circular(10)
-                              ),
-                              child: const Padding(
-                                padding: EdgeInsets.symmetric(vertical: 17, horizontal: 90),
-                                child: Text(
-                                  "Subscribe",
-                                  style: TextStyle(
-                                    fontSize: 17,
-                                    color: Colors.white
-                                  ),
-                                ),
-                              ),
-                            ),
-                          )
-                    ],
-                  ),
-                ),
+                      ),
+                    ]
+                    ),
+                  )
+              ]
               ),
-            ),
           ),
         ),
       ),
